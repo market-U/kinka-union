3ed3c4ef2d7c6796be5a0fdb1b11996e071966cd
t u <tommy@makumini.local>
2022-08-04 20:24:46 +0900

    setup plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/typescript/recommended", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": 0,
   },
 };
