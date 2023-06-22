4ce09140d333fa07fcb86df00b09776e49fe0c39
t u <tommy@makumini.local>
2023-06-22 23:52:59 +0900

    debug

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  outputDir: "dist",
-  publicPath: "/kinka-union",
+  outputDir: "docs",
 });
